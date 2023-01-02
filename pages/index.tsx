0e36d7b9717486cbb05fbb8f3ab0261e7a2dfe28
samueeli <samuli.salminen@raivo.io>
2023-01-02 17:10:17 +0200

    added types + serverprops. Some fixes with sanity v2 to v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
 import HeroBanner from '../components/HeroBanner';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
+
+// 1:05:58
 
 // To learn how commands have changed from Sanity Studio v2 to v3, check:        ║
 // https://www.sanity.io/help/studio-v2-vs-v3
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home = ({ posts }: Props) => {
+  console.log('samulin postit', posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -17,8 +26,34 @@ const Home: NextPage = () => {
 
       <Header />
       <HeroBanner />
+
+      {/* Posts */}
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    slug,
+    author -> {
+      name,
+      image
+    },
+    mainImage,
+    description,
+  }
+  `;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
