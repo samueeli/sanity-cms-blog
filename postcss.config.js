ccbf869432879620ae654027f52ba216cde99f7d
samueeli <samuli.salminen@raivo.io>
2023-01-02 12:16:16 +0200

    added header, hero, plus temporary fixes to make sanity work

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    },
     autoprefixer: {},
   },
-}
+};
